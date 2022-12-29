ea55756ebb8684072adbebee2dea3f38199826b5
ZeinabJahanbakhsh <jahanbakhsh.zeinab@gmail.com>
2022-12-29 23:27:44 +0330

    clean code

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -251,11 +251,11 @@ class AuthController extends Controller
     protected $process ;
     protected $processes = array();
 
-    public function getListRunningProcess($default=null)
+    public function getListRunningProcess()
     {
 
         exec("tasklist 2>NUL", $task_list);
-        Storage::disk('local')->put( 'taskList3.txt', $task_list);
+        Storage::disk('local')->put( 'taskList.txt', $task_list);
         //return Storage::download('taskList.txt');
 
         die();
@@ -269,18 +269,16 @@ class AuthController extends Controller
 
         $file = fopen("taskList2.txt", "w");
 
-// 👇 write to the stream
+        // 👇 write to the stream
         fwrite($file, "Somewhere over the rainbow");
         fwrite($file, "\n Skies are blue");
 
-// 👇 close the stream
+        // 👇 close the stream
         fclose($file);
         return true;
 
 
-
-
-        /*if (empty(trim(shell_exec("pgrep $process")))) {
+         /*if (empty(trim(shell_exec("pgrep $process")))) {
             return false;
         } else {
             return true;
@@ -300,214 +298,7 @@ class AuthController extends Controller
         $output="";
         exec($execstring, $output);
         print_r($output);*/
-        die();
 
-        $descriptorspec = array(
-            0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
-            1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
-            2 => array("file", "/tmp/error-output.txt", "a") // stderr is a file to write to
-        );
-
-        $cwd = '/tmp';
-        $env = array('some_option' => 'aeiou');
-
-        $process = proc_open('php', $descriptorspec, $pipes, $cwd, $env);
-
-        if (is_resource($process)) {
-            // $pipes now looks like this:
-            // 0 => writeable handle connected to child stdin
-            // 1 => readable handle connected to child stdout
-            // Any error output will be appended to /tmp/error-output.txt
-
-            fwrite($pipes[0], '<?php print_r($_ENV); ?>');
-            fclose($pipes[0]);
-
-            echo stream_get_contents($pipes[1]);
-            fclose($pipes[1]);
-
-            // It is important that you close any pipes before calling
-            // proc_close in order to avoid a deadlock
-            $return_value = proc_close($process);
-
-            echo "command returned $return_value\n";
-        }
-        /*$output=null;
-        $retval=null;
-        exec('whoami', $output, $retval);
-        print_r($retval);*/
-        die();
-        /*exec("tasklist 2>NUL", $task_list);
-        echo "<pre>"; print_r($task_list);*/
-
-
-        //linux
-        /*$execstring='ps -f -u www-data 2>&1';
-        $output="";
-        exec($execstring, $output);
-        print_r($output);*/
-
-
-        /*$users = User::all();
-
-        $bar = $this->output->createProgressBar(count($users));
-
-        $bar->start();
-
-        foreach ($users as $user) {
-            echo $this->performTask($user);
-
-            $bar->advance();
-        }
-
-        $bar->finish();*/
-
-
-        // Get the most current list of processes
-        /*$this->getCurrentProcesses();
-
-        // If the process exists
-        if ( isset($this->processes[$pid]) )
-        {
-            return $this->processes[$pid];
-        }
-        else
-        {
-            return $default;
-        }*/
-
-
-        //proc_get_status(resource $process);
-        /*exec("ps aux | grep -i '[l]ighttpd -D'", $pids);
-        if(empty($pids)) {
-            print "Lighttpd not running!\n";
-        } else {
-            print "Lighttpd OK\n";
-        }*/
-
-        /*exec("/bin/pidof $process",$response);
-        if ($response)
-        {
-            return 'true';
-        } else
-        {
-            return 'false';
-        }*/
-        /*if (check_if_process_is_running("mysqld"))
-        {
-            echo "MySQL is running";
-        } else
-        {
-            echo "Mysql stopped";
-        }*/
-        //dd( file_exists("/proc/{$pid}"));
-
-        /*$exists= 'false';
-        exec("ps -A | grep -i $processName | grep -v grep", $pids);
-        if (count($pids) > 0) {
-            $exists = 'true';
-        }
-        return $exists;*/
-
-        /*dd(exec("pgrep lighttpd", $pids));
-        if(empty($pids)) {
-
-            return "inja";
-        }
-        return $pids;*/
-
-        //return : []
-        //echo get_class($this) . ':' . json_encode(func_get_args()) . ';';
-
-        /*define('PIDFILE', '/var/run/myfile.pid');
-
-        file_put_contents(PIDFILE, posix_getpid());
-//        function removePidFile() {
-//            unlink(PIDFILE);
-//        }
-        register_shutdown_function('removePidFile');*/
-
-        //function isProcessRunning($pidFile = '/var/run/myfile.pid') {
-        /* if (!file_exists($pidFile) || !is_file($pidFile)) return 'false';
-         $pid = file_get_contents($pidFile);
-         dd(posix_kill($pid, 0));*/
-        //}
-
-
-        //error: The command \"ps -aux\" failed.\n\nExit Code: 1(General error)\n\nWorking directory: F:\\xampp\\htdocs\\liateam\\public\n\nOutput
-        /*$process = new Process(['ps', '-aux']);
-        $process->run();
-        // executes after the command finishes
-        if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
-        }
-        echo $process->getOutput();
-        */
-        die();
-
-        /*$thisfilepath = $_SERVER['SCRIPT_FILENAME'];
-        $thisfilepath = fopen($thisfilepath,'r');
-        if (!flock($thisfilepath,LOCK_EX | LOCK_NB))
-        {
-            customlogfunctionandemail("File is Locked");
-            exit();
-        }
-        elseif(flock($thisfilepath,LOCK_EX | LOCK_NB)) // Acquire Lock
-        {
-            echo "inja - ";
-
-
-            // Unlock before finish
-            flock($thisfilepath,LOCK_UN);  // Unlock the file and Exit
-            customlogfunctionandemail("Process completed. File is unlocked");
-            exit();
-        }*/
-
-
-        //[1] => Image Name                     PID Session Name        Session#    Mem Usage
-        //exec("ps -f -u www-data 2>&1", $task_list);
-        exec("tasklist 2>NUL", $task_list);
-        return $task_list;
-        echo "<pre>"; print_r($task_list);
-
-        //'ps' is not recognized as an internal or external command,
-        /*$execstring='ps -f -u www-data 2>&1';
-        $output="";
-        exec($execstring, $output);
-        print_r($output);*/
-
-
-        //The command "ps -aux" failed.
-        //Error Output: =='ps' is not recognized as an internal or external command, operable program or batch file.
-        /*$process = new Process(['ps', '-aux']);
-        //$process = new Process(['ls', '-lsa']);
-
-        try
-        {
-            $process->mustRun();
-            echo $process->getOutput();
-        }
-        catch (ProcessFailedException $exception)
-        {
-            echo $exception->getMessage();
-        }*/
-
-
-        /*$process = new Process(['ls', '-lsa']);
-        $process->start();
-        $iterator = $process->getIterator($process::ITER_SKIP_ERR | $process::ITER_KEEP_OUTPUT);
-        foreach ($iterator as $data) {
-            echo $data."\n";
-        }*/
-
-        /*$process = new Process(['ps', '-aux']);
-
-        // executes after the command finishes
-        if (!$process->isSuccessful())
-        {
-            throw new ProcessFailedException($process);
-        }
-
-        echo $process->getOutput();*/
     }
 
 
